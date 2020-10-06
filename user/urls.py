2f4bc0f31caa99e956e57235570bb32df3c77c66
timjonez <timjonez@protonmail.com>
2020-10-06 16:27:08 -0500

    [user/urls] add login/logout urls

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from .views import SignUpView
 
 app_name = 'user'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
