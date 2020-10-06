67955fc6531d954f9037970bc454abbaad5d2c77
timjonez <timjonez@protonmail.com>
2020-10-06 16:41:11 -0500

    [urls] add url pattern for home page

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import HomeView
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls', namespace='user')),
 ]
