c18b04b5b47143574c1b04d722fa06e8cf864e66
timjonez <timjonez@protonmail.com>
2020-10-06 15:14:33 -0500

    [user] create user app with custom user model

---
+++ user/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import CustomUserManager
+
+WORKER = ''
+CHURCH = ''
+ROLE = [
+    (WORKER, 'Volunteer'),
+    (CHURCH, 'Church'),
+]
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    role = models.CharField(choices=ROLE, max_length=12, default=WORKER)
+
+    REQUIRED_FIELDS = []
+
+    USERNAME_FIELD = 'email'
+    objects = CustomUserManager()
+
