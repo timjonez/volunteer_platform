4021aaa6f14b0e820e66254409ec6ba6de30ead5
timjonez <timjonez@protonmail.com>
2020-10-06 16:07:10 -0500

    [users/views] create signup view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import User
+from .forms import CreateUserForm
+
+
+class SignUpView(CreateView):
+    form_class = CreateUserForm
+    success = reverse_lazy('login')
+    template_name = 'user/signup.html'
+
