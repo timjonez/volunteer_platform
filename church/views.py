7b3ab63e761bbf8bf2740b622820ea12e0564ac0
timjonez <timjonez@protonmail.com>
2020-10-07 17:22:28 -0500

    [church/views] create view to add a church

--- church/views.py
+++ church/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import AddChurchForm
+from .models import Church
+
+
+def create_church_view(request):
+    if request.method == 'GET':
+        form = AddChurchForm()
+    else:
+        form = AddChurchForm(request.POST, request.FILES)
+        if form.is_valid():
+            church = form.save(commit=False)
+            church.user = request.user
+            church.save()
+            return redirect('home')
+    return render(request, 'church/add_church.html', {'form': form,})
+
