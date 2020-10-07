8283811411a7ab4226126c87c777f99f9222d499
timjonez <timjonez@protonmail.com>
2020-10-07 17:20:08 -0500

    [church/model] allow image to be blank, and and string rep function

--- church/models.py
+++ church/models.py
@@ -9,10 +9,13 @@ from user.models import User
 class Church(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    picture = models.ImageField(upload_to='uploaded')
+    picture = models.ImageField(blank=True)
     description = models.TextField(max_length=1000, blank=True)
     pastor_name = models.CharField(max_length=254)
     phone = PhoneField(help_text='Phone Number')
     address = AddressField(on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
