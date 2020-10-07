4260570c918d6ccfb9aa2da23c91808097ea63d9
timjonez <timjonez@protonmail.com>
2020-10-07 13:57:00 -0500

    [settings] add additional apps to settings

--- volunteer/settings.py
+++ volunteer/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'phone_field',
+    'address',
+
     'user',
     'church',
 ]
