b48c19e2b95af7b33cfafece8297dc74c7813c43
timjonez <timjonez@protonmail.com>
2020-10-07 17:26:55 -0500

    [urls] add church urls, and media urls

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls', namespace='user')),
+    path('church/', include('church.urls', namespace='church'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
