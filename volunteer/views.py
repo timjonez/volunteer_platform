62508a0dd24ef9ae87e6db864a19ff2e63d740cb
timjonez <timjonez@protonmail.com>
2020-10-08 08:25:51 -0500

    [views] change home view into a list view

--- volunteer/views.py
+++ volunteer/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from Job.models import Job
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Job
     template_name = 'home.html'
