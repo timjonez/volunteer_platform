8259db923bdcd33b29c025b987246e36d7c4421c
timjonez <timjonez@protonmail.com>
2020-10-07 21:33:13 -0500

    [job/views] add view to create jobs

--- Job/views.py
+++ Job/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
 
-# Create your views here.
+from church.models import Church
+from .models import Job
+from .forms import CreateJobForm
+
+
+def create_job_view(request):
+    church = Church.objects.filter(user_id__email=request.user)
+    if len(church) != 1:
+        return redirect('home')
+    elif request.method == 'GET':
+        form = CreateJobForm()
+    else:
+        form = CreateJobForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.church = church[0]
+            job.save()
+            return redirect('home')
+    return render(request, 'Job/add_job.html', {'form': form,})
