2f528ddaf72519484ce3387aa143101b14ca6da5
timjonez <timjonez@protonmail.com>
2020-10-08 11:23:43 -0500

    [job/models] add slug field and set up url method

--- Job/models.py
+++ Job/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
 from church.models import Church
 
@@ -18,9 +20,17 @@ class Job(models.Model):
     created_date = models.DateTimeField(auto_now=True)
     duration = models.CharField(choices=DURATION, max_length=50, default='Once')
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     body = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return 'By ' + self.church.name +': '+ self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.church.name +'-'+ self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return 'jobs/' + self.slug
 
