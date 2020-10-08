36b167f8b88ac6a1afb4755a301d635824745620
timjonez <timjonez@protonmail.com>
2020-10-07 19:03:00 -0500

    [job/models] add job and category models

--- Job/models.py
+++ Job/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from church.models import Church
+
+class Category(models.Model):
+    category_item = models.CharField(max_length=100, help_text='Category')
+
+    def __str__(self):
+        return self.category_item
+    
+DURATION = [
+    ('Once', 'One Time'),
+    ('Long', 'Long-term')
+]
+
+class Job(models.Model):
+    church = models.ForeignKey(Church, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now=True)
+    duration = models.CharField(choices=DURATION, max_length=50, default='Once')
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
