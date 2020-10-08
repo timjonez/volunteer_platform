0370276ca9aa1523caa41b0c886e1a1b85d02619
timjonez <timjonez@protonmail.com>
2020-10-08 14:51:12 -0500

    [views] add categories to home view

--- volunteer/views.py
+++ volunteer/views.py
@@ -1,7 +1,12 @@
 from django.views.generic import ListView
-from Job.models import Job
+from Job.models import Job, Category
 
 
 class HomeView(ListView):
     model = Job
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
