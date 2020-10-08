ca27e41d26060671e02acc26a6a7b8cba7aef362
timjonez <timjonez@protonmail.com>
2020-10-07 21:45:04 -0500

    [church/views] add limit check to add church view

--- church/views.py
+++ church/views.py
@@ -6,7 +6,10 @@ from .models import Church
 
 
 def create_church_view(request):
-    if request.method == 'GET':
+    existing_churches = Church.objects.filter(user_id__email=request.user)
+    if len(existing_churches) != 0:
+        return redirect('home')
+    elif request.method == 'GET':
         form = AddChurchForm()
     else:
         form = AddChurchForm(request.POST, request.FILES)
@@ -16,4 +19,3 @@ def create_church_view(request):
             church.save()
             return redirect('home')
     return render(request, 'church/add_church.html', {'form': form,})
-
