9052410e7d51586b8634e380b014e666ba992aa8
timjonez <timjonez@protonmail.com>
2020-10-08 11:25:37 -0500

    [job/urls] add url pattern for detail view

--- Job/urls.py
+++ Job/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_job_view
+from .views import create_job_view, JobDetailView
 
 app_name = 'Job'
 
 urlpatterns = [
     path('create_job/', create_job_view, name='create_job'),
+    path('<slug:slug>/', JobDetailView.as_view(), name='view_job'),
 ]
