d3a74666207743711329fbcd677cd669201c39b0
timjonez <timjonez@protonmail.com>
2020-10-11 16:23:15 -0500

    [user/urls] add url pattern to add volunteer

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import SignUpView
+from .views import SignUpView, create_volunteer_view
 
 app_name = 'user'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/volunteer/', create_volunteer_view, name='volunteer'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
