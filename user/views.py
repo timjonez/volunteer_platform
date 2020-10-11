77f6eeacfe1030915574a7d0b9f009cf2b83e5c2
timjonez <timjonez@protonmail.com>
2020-10-11 16:22:37 -0500

    [user/views] add view to add volunteer object to user

--- user/views.py
+++ user/views.py
@@ -12,3 +12,16 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('user:login')
     template_name = 'user/signup.html'
 
+
+def create_volunteer_view(request):
+    user = User.objects.filter(email=request.user)
+    if request.method == 'GET':
+        form = CreateVolunteerForm()
+    else:
+        form = CreateVolunteerForm(request.POST, request.FILES)
+        if form.is_valid():
+            volunteer = form.save(commit=False)
+            volunteer.user = user[0]
+            volunteer.save()
+            return redirect('home')
+    return render(request, 'user/add_volunteer.html', {'form': form,})
