48aaccb1be1cec86494f25bff7840d6b43fc2d5e
timjonez <timjonez@protonmail.com>
2020-10-11 16:20:58 -0500

    [user/forms] add form to create volunteer object

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,6 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User, Volunteer
 
 
 class CreateUserForm(UserCreationForm):
@@ -7,3 +8,10 @@ class CreateUserForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = User
         fields = ('first_name', 'last_name', 'role', 'email', 'password1', 'password2')
+
+
+class CreateVolunteerForm(forms.ModelForm):
+
+    class Meta:
+        model = Volunteer
+        exclude = ['user',]
