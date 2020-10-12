20d4bd7bec3d8967c1a1f88f3d56c7e4c5f21811
timjonez <timjonez@protonmail.com>
2020-10-12 12:02:14 -0500

    [views] change home view to redirect when signup in not complete

--- volunteer/views.py
+++ volunteer/views.py
@@ -1,12 +1,32 @@
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
-from Job.models import Job, Category
+
+from Job.models import Job, Category, Volunteer
+from church.models import Church
 
 
-class HomeView(ListView):
-    model = Job
-    template_name = 'home.html'
+def home_view(request):
+    user = request.user
+    if not request.user.is_authenticated:
+        context = {
+            'object_list':Job.objects.all(),
+            'categories': Category.objects.all()
+        }
+    else:
+        if user.role == 'Church':
+            church = Church.objects.filter(user_id__email=user.email)
+            if len(church) == 0:
+                return redirect('church:add_church')
+        elif user.role == 'Volunteer':
+            volunteer = Volunteer.objects.filter(user_id__email=user.email)
+            if len(volunteer) == 0:
+                return redirect('user:volunteer')
+        else:
+            context = {
+            'object_list':Job.objects.all(),
+            'categories': Category.objects.all()
+        }
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        return context
+
+    return render(request, 'home.html', context)
+
