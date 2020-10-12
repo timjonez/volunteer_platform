586e7551a7c8bd1780054a4b1f61796915d0baa6
timjonez <timjonez@protonmail.com>
2020-10-12 10:42:58 -0500

    [job/admin] add proposal model to admin

--- Job/admin.py
+++ Job/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Job, Category
+from .models import Job, Category, Proposal
 
 admin.site.register(Job)
 admin.site.register(Category)
+admin.site.register(Proposal)
