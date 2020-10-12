719859c01b80f784259cbf5a8cb889a45cd0c7fd
timjonez <timjonez@protonmail.com>
2020-10-12 10:42:21 -0500

    [job/urls] add url pattern to create proposal

--- Job/urls.py
+++ Job/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_job_view, JobDetailView
+from .views import create_job_view, JobDetailView, create_proposal_view
 
 app_name = 'Job'
 
 urlpatterns = [
+    path('create-proposal/<slug:slug>/', create_proposal_view, name='create_proposal'),
     path('create_job/', create_job_view, name='create_job'),
     path('<slug:slug>/', JobDetailView.as_view(), name='view_job'),
 ]
