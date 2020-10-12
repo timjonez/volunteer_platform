cd5b8718316b6984082e5d9a189694de7996ece3
timjonez <timjonez@protonmail.com>
2020-10-12 10:40:49 -0500

    [job/forms] create form for the proposal model

--- Job/forms.py
+++ Job/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Job
+from .models import Job, Proposal
 
 
 class CreateJobForm(forms.ModelForm):
@@ -7,3 +7,10 @@ class CreateJobForm(forms.ModelForm):
     class Meta:
         model = Job
         exclude = ['church', 'slug',]
+
+
+class CreateProposalForm(forms.ModelForm):
+
+    class Meta:
+        model = Proposal
+        exclude = ['user', 'job',]
