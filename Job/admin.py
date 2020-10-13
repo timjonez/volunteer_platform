1ccdfea1fa2944f6919ef1c5021755d46449ac20
timjonez <timjonez@protonmail.com>
2020-10-13 13:04:35 -0500

    [job/admin] add savedjob model to admin

--- Job/admin.py
+++ Job/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Job, Category, Proposal
+from .models import Job, Category, Proposal, SavedJob
 
 admin.site.register(Job)
 admin.site.register(Category)
 admin.site.register(Proposal)
+admin.site.register(SavedJob)
