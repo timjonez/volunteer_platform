6636ced523b1c4fe695e821e4080f57ffbe6a996
timjonez <timjonez@protonmail.com>
2020-10-13 13:06:13 -0500

    [job/urls] add url patterns for savedjob views

--- Job/urls.py
+++ Job/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('create-proposal/<slug:slug>/', views.create_proposal_view, name='create_proposal'),
     path('create_job/', views.create_job_view, name='create_job'),
     path('proposals/', views.ProposalListView.as_view(), name='proposals'),
+    path('saved/', views.SavedJobListView.as_view(), name='saved-jobs'),
+    path('save-job/<slug:slug>/', views.save_job_view, name='save-job'),
     path('<slug:slug>/', views.JobDetailView.as_view(), name='view_job'),
 ]
