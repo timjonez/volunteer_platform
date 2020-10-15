a2b84033196ce34c88111829aa7a1a32c5e6a10b
timjonez <timjonez@protonmail.com>
2020-10-15 10:22:43 -0500

    [job/urls] change url patterns to match new views

--- Job/urls.py
+++ Job/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from . import views #create_job_view, JobDetailView, create_proposal_view,
+from . import views
 
 app_name = 'Job'
 
 urlpatterns = [
     path('create-proposal/<slug:slug>/', views.create_proposal_view, name='create_proposal'),
     path('create_job/', views.create_job_view, name='create_job'),
-    path('proposals/', views.ProposalListView.as_view(), name='proposals'),
-    path('saved/', views.SavedJobListView.as_view(), name='saved-jobs'),
+    path('proposals/', views.proposal_list_view, name='proposals'),
+    path('saved/', views.saved_job_list_view, name='saved-jobs'),
     path('save-job/<slug:slug>/', views.save_job_view, name='save-job'),
     path('<slug:slug>/', views.JobDetailView.as_view(), name='view_job'),
 ]
