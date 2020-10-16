42274a64ca59fe4003c5857049cb4be9ebdd5461
timjonez <timjonez@protonmail.com>
2020-10-16 17:44:39 -0500

    [user/urls] add url pattern for edit volunteer profile page

--- user/urls.py
+++ user/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('volunteer/<slug>/', views.VolunteerDetailView.as_view(), name='see-profile'),
+    path('volunteer/<email>/edit/', views.volunteer_edit_view, name='volunteer-edit'),
 ]
