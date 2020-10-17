42c97a2942161c4f183ed80685fb9e8835b09e24
timjonez <timjonez@protonmail.com>
2020-10-17 10:36:37 -0500

    [church/urls] add url pattern for church detail view

--- church/urls.py
+++ church/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_church_view
+from . import views
 
 app_name = 'church'
 
 
 urlpatterns = [
-    path('add/', create_church_view, name='add_church'),
+    path('add/', views.create_church_view, name='add_church'),
+    path('<email>/', views.church_detail_view, name='profile'),
 ]
