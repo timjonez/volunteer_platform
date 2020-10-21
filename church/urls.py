82da2ccdd258a9543b27160a75b98961a0ee3ffa
timjonez <timjonez@protonmail.com>
2020-10-20 21:29:05 -0500

    [church/urls] change url patterns to use slug

--- church/urls.py
+++ church/urls.py
@@ -6,6 +6,6 @@ app_name = 'church'
 
 urlpatterns = [
     path('add/', views.create_church_view, name='add_church'),
-    path('<email>/edit/', views.church_edit_view, name='edit-church'),
-    path('<email>/', views.church_detail_view, name='profile'),
+    path('<slug:slug>/edit/', views.church_edit_view, name='edit-church'),
+    path('<slug:slug>/', views.church_detail_view, name='profile'),
 ]
